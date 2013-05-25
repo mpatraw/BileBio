7d09b7163881bb5e2703a6f6799d2877d7e4d6dc
Michael Patraw <xjabberwocky@gmail.com>
2013-05-25 00:57:45 -0700

    Forgot 2 files.

---
+++ src/game/player.hpp
@@ -0,0 +1,66 @@
+
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include <boost/noncopyable.hpp>
+
+#include <entity.hpp>
+#include <plants.hpp>
+
+struct attributes
+{
+    ssize_t energy;
+    ssize_t max_energy;
+    ssize_t damage;
+    double chance_to_hit;
+};
+
+class player : public entity, private boost::noncopyable
+{
+public:
+    enum action
+    {
+        act_none,
+        act_move,
+    };
+
+    player(region *reg, plant_manager *pm) :
+        entity(reg), plant_manager_(pm)
+    {
+        attributes_ = {3, 3, 2, 0.66};
+    }
+    virtual ~player() { }
+
+    virtual const ssize_t &get_x() const { return x_; }
+    virtual ssize_t &get_x() { return x_; }
+
+    virtual const ssize_t &get_y() const { return y_; }
+    virtual ssize_t &get_y() { return y_; }
+
+    virtual bool move_by(ssize_t dx, ssize_t dy)
+    {
+        return move_to(x_ + dx, y_ + dy);
+    }
+
+    virtual bool move_to(ssize_t x, ssize_t y)
+    {
+        if (region_->in_bounds(x, y) && region_->tile_at(x, y) >= t_dirt)
+        {
+            x_ = x;
+            y_ = y;
+            return true;
+        }
+        return false;
+    }
+
+    virtual const attributes &get_attributes() const { return attributes_; }
+    virtual attributes &get_attributes() { return attributes_; }
+
+protected:
+    ssize_t x_;
+    ssize_t y_;
+    plant_manager *plant_manager_;
+    attributes attributes_;
+};
+
+#endif
---
+++ src/game/random.hpp
@@ -0,0 +1,42 @@
+
+#ifndef RANDOM_HPP
+#define RANDOM_HPP
+
+#include <ctime>
+#include <random>
+
+class rng
+{
+public:
+    rng(size_t seed=0)
+        : gen_(rd_())
+    {
+        set_seed(seed);
+    }
+
+    size_t get_seed() const { return seed_; }
+    void set_seed(size_t seed)
+    {
+        if (!seed)
+            seed = std::time(nullptr);
+        gen_.seed(seed);
+    }
+
+    double get_uniform()
+    {
+        std::uniform_real_distribution<double> dis(0, 1);
+        return dis(gen_);
+    }
+
+    ssize_t get_range(ssize_t min, ssize_t max)
+    {
+        std::uniform_int_distribution<ssize_t> dis(min, max);
+        return dis(gen_);
+    }
+private:
+    size_t seed_;
+    std::random_device rd_;
+    std::mt19937_64 gen_;
+};
+
+#endif
